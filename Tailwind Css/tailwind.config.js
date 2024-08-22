c1c9123b8e07a9222bf7857fbd80f3bb5be28e43
mrt786 <muhammedrehantariq786@gmail.com>
2024-08-22 21:54:06 +0500

    Tailwind Installtion

---
+++ Tailwind Css/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.html",  // Ensure this matches the structure of your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
